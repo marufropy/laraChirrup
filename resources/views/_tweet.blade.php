92341e99159a7e58afa9ce696752c7d89c23c97e
marufropy <marufropy@gmail.com>
2020-09-22 15:12:38 +0600

    upto timeline design

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-grey-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://picsum.photos/50" alt="avatar" class="rounded-full mr-2">
+    </div>
+
+    <div>
+        <h5 class="font-bold-mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, perferendis amet aliquid, quisquam praesentium suscipit, reprehenderit eos incidunt labore aperiam expedita. Aliquid iure, vero ab sequi laborum numquam dolor est?
+        </p>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,6 +63,7 @@
             }
         </style>
     </head>
+    
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
