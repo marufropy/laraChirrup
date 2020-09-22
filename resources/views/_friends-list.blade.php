1ab75868189575446dd28c74eee0a4b559857d4e
marufropy <marufropy@gmail.com>
2020-09-22 21:07:25 +0600

    upto follow building

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,12 +1,15 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<div class="bg-gray-200 rounded-lg py-4 px-6">
+    <h3 class="font-bold text-xl mb-4">Friends</h3>
 
-<ul>
-    @foreach (range(1,8) as $index)
-        <li class="mb-4">
-            <div class="flex items-center text-sm">
-                <img src="https://picsum.photos/seed/picsum/40" alt="avatar" class="rounded-full mr-2">
-                John Doe
-            </div>
-        </li>        
-    @endforeach
-</ul>
+    <ul>
+        @foreach (auth()->user()->follows as $user)
+            <li class="mb-4">
+                <div class="flex items-center text-sm">
+                    {{-- <img src="https://picsum.photos/seed/picsum/40" alt="avatar" class="rounded-full mr-2"> --}}
+                    <img src="{{$user->avatar}}" alt="avatar" class="rounded-full mr-2">
+                    {{$user->name}}
+                </div>
+            </li>        
+        @endforeach
+    </ul>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,9 @@
             </div>
         </div>
 
-        <div class="lg:w=1/6 bg-blue-100 rounder-lg p-4">
-            @include('_friends-list')
-        </div>
+        {{-- <div class="lg:w=1/6 bg-blue-100 rounded-lg p-4">
+            
+        </div> --}}
+        @include('_friends-list')
     </div>
 @endsection
